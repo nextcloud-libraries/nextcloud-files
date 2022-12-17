57c9b20cda87c0f4e18913ff8e172d1533e578d4
Zipdox <42157341+Zipdox@users.noreply.github.com>
2022-12-17 16:47:56 +0100

    Fix decimal prefixes and add option for binary prefixes per IEC 80000-13

--- __tests__/humanFileSize.spec.ts
+++ __tests__/humanFileSize.spec.ts
@@ -9,52 +9,98 @@ declare global {
 describe('humanFileSize', () => {
     describe('formatFileSize', () => {
         it('renders file sizes with the correct unit', function() {
-            const data = [
+            const dataDecimal = [
                 [0, '0 B'],
                 ["0", '0 B'],
                 ["A", 'NaN B'],
                 [125, '125 B'],
-                [128000, '125 KB'],
-                [128000000, '122.1 MB'],
-                [128000000000, '119.2 GB'],
-                [128000000000000, '116.4 TB'],
-                [128000000000000.0, '116.4 TB'],
+                [125000, '125 KB'],
+                [122100000, '122.1 MB'],
+                [119200000000, '119.2 GB'],
+                [116400000000000, '116.4 TB'],
+                [116400000000000.0, '116.4 TB'],
+                [113700000000000000.0, '113.7 PB'],
             ]
-            for (var i = 0; i < data.length; i++) {
-                expect(formatFileSize(data[i][0])).toEqual(data[i][1])
+            const dataBinary = [
+                [0, '0 B'],
+                ["0", '0 B'],
+                ["A", 'NaN B'],
+                [125, '125 B'],
+                [128000, '125 KiB'],
+                [128000000, '122.1 MiB'],
+                [128000000000, '119.2 GiB'],
+                [128000000000000, '116.4 TiB'],
+                [128000000000000.0, '116.4 TiB'],
+                [128000000000000000.0, '113.7 PiB'],
+            ]
+            for (var i = 0; i < dataDecimal.length; i++) {
+                expect(formatFileSize(dataDecimal[i][0])).toEqual(dataDecimal[i][1])
+            }
+            for (var i = 0; i < dataBinary.length; i++) {
+                expect(formatFileSize(dataBinary[i][0], false, true)).toEqual(dataBinary[i][1])
             }
         })
 
         it('renders file sizes with the correct unit for small sizes', function() {
-            var data = [
+            var dataDecimal = [
                 [0, '0 KB'],
                 [125, '< 1 KB'],
-                [128000, '125 KB'],
-                [128000000, '122.1 MB'],
-                [128000000000, '119.2 GB'],
-                [128000000000000, '116.4 TB'],
-                [128000000000000.0, '116.4 TB'],
+                [125000, '125 KB'],
+                [122100000, '122.1 MB'],
+                [119200000000, '119.2 GB'],
+                [116400000000000, '116.4 TB'],
+                [116400000000000.0, '116.4 TB'],
+                [113700000000000000.0, '113.7 PB'],
             ]
-            for (var i = 0; i < data.length; i++) {
-                expect(formatFileSize(data[i][0], true)).toEqual(data[i][1])
+            var dataBinary = [
+                [0, '0 KiB'],
+                [125, '< 1 KiB'],
+                [128000, '125 KiB'],
+                [128000000, '122.1 MiB'],
+                [128000000000, '119.2 GiB'],
+                [128000000000000, '116.4 TiB'],
+                [128000000000000.0, '116.4 TiB'],
+                [128000000000000000.0, '113.7 PiB'],
+            ]
+            for (var i = 0; i < dataDecimal.length; i++) {
+                expect(formatFileSize(dataDecimal[i][0], true)).toEqual(dataDecimal[i][1])
+            }
+            for (var i = 0; i < dataBinary.length; i++) {
+                expect(formatFileSize(dataBinary[i][0], true, true)).toEqual(dataBinary[i][1])
             }
         })
 
         it('renders file sizes with the correct locale', function() {
             document.documentElement.dataset.locale = 'de'
-            const data = [
+            const dataDecimal = [
                 [0, '0 B'],
                 ["0", '0 B'],
                 ["A", 'NaN B'],
                 [125, '125 B'],
-                [128000, '125 KB'],
-                [128000000, '122,1 MB'],
-                [128000000000, '119,2 GB'],
-                [128000000000000, '116,4 TB'],
-                [128000000000000.0, '116,4 TB'],
+                [125000, '125 KB'],
+                [122100000, '122,1 MB'],
+                [119200000000, '119,2 GB'],
+                [116400000000000, '116,4 TB'],
+                [116400000000000.0, '116,4 TB'],
+                [113700000000000000.0, '113,7 PB'],
             ]
-            for (var i = 0; i < data.length; i++) {
-                expect(formatFileSize(data[i][0])).toEqual(data[i][1])
+            const dataBinary = [
+                [0, '0 B'],
+                ["0", '0 B'],
+                ["A", 'NaN B'],
+                [125, '125 B'],
+                [128000, '125 KiB'],
+                [128000000, '122,1 MiB'],
+                [128000000000, '119,2 GiB'],
+                [128000000000000, '116,4 TiB'],
+                [128000000000000.0, '116,4 TiB'],
+                [128000000000000000.0, '113,7 PiB'],
+            ]
+            for (var i = 0; i < dataDecimal.length; i++) {
+                expect(formatFileSize(dataDecimal[i][0])).toEqual(dataDecimal[i][1])
+            }
+            for (var i = 0; i < dataBinary.length; i++) {
+                expect(formatFileSize(dataBinary[i][0], false, true)).toEqual(dataBinary[i][1])
             }
         })
     })
--- lib/humanfilesize.ts
+++ lib/humanfilesize.ts
@@ -23,7 +23,8 @@
 
 import { getCanonicalLocale } from '@nextcloud/l10n'
 
-const humanList = ['B', 'KB', 'MB', 'GB', 'TB'];
+const humanList = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+const humanListBinary = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB'];
 
 /**
  * Format a file size in a human-like format. e.g. 42GB
@@ -31,25 +32,27 @@ const humanList = ['B', 'KB', 'MB', 'GB', 'TB'];
  * @param size in bytes
  * @param skipSmallSizes avoid rendering tiny sizes and return '< 1 KB' instead
  */
-export function formatFileSize(size: number|string, skipSmallSizes: boolean = false): string {
+export function formatFileSize(size: number|string, skipSmallSizes: boolean = false, binaryPrefixes: boolean = false): string {
 
 	if (typeof size === 'string') {
 		size = Number(size)
 	}
 
-	// Calculate Log with base 1024: size = 1024 ** order
-	let order = size > 0 ? Math.floor(Math.log(size) / Math.log(1024)) : 0;
+	/*
+	* @note This block previously used Log base 1024, per IEC 80000-13;
+	* however, the wrong prefix was used. Now we use decimal calculation
+	* with base 1000 per the SI. Base 1024 calculation with binary
+	* prefixes is optional, but has yet to be added to the UI.
+	*/
+	// Calculate Log with base 1024 or 1000: size = base ** order
+	let order = size > 0 ? Math.floor(Math.log(size) / Math.log(binaryPrefixes ? 1024 : 1000)) : 0;
 	// Stay in range of the byte sizes that are defined
-	order = Math.min(humanList.length - 1, order);
-	const readableFormat = humanList[order];
-	let relativeSize = (size / Math.pow(1024, order)).toFixed(1);
+	order = Math.min((binaryPrefixes ? humanListBinary.length : humanList.length) - 1, order);
+	const readableFormat = binaryPrefixes ? humanListBinary[order] : humanList[order];
+	let relativeSize = (size / Math.pow(binaryPrefixes ? 1024 : 1000, order)).toFixed(1);
 
 	if (skipSmallSizes === true && order === 0) {
-		if (relativeSize !== '0.0') {
-			return '< 1 KB';
-		} else {
-			return '0 KB';
-		}
+		return (relativeSize !== '0.0' ? '< 1 ' : '0 ') + (binaryPrefixes ? humanListBinary[1] : humanList[1]);
 	}
 
 	if (order < 2) {
