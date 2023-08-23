d11d0bb17a01cf44f46f3d037f0271621c96d905
Ferdinand Thiessen <opensource@fthiessen.de>
2023-08-23 16:40:45 +0200

    fix(davResultToNode): The root is already part of the filename so do not double include in node source
    Co-authored-by: John Molakvoæ <skjnldsv@users.noreply.github.com>
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- __tests__/dav/dav.spec.ts
+++ __tests__/dav/dav.spec.ts
@@ -1,7 +1,8 @@
 import { afterAll, describe, expect, test, vi } from 'vitest'
 import { readFile } from 'node:fs/promises'
 
-import { File, Folder, davRemoteURL, davGetFavoritesReport, davRootPath, getFavoriteNodes } from '../../lib'
+import { File, Folder, davRemoteURL, davGetFavoritesReport, davRootPath, getFavoriteNodes, davResultToNode } from '../../lib'
+import { FileStat } from 'webdav'
 
 vi.mock('@nextcloud/auth')
 vi.mock('@nextcloud/router')
@@ -17,6 +18,63 @@ describe('DAV functions', () => {
 
 	test('remote url is correct', () => {
 		expect(davRemoteURL).toBe('https://localhost/dav')
+	})
+})
+
+describe('davResultToNode', () => {
+	/* Result of:
+	davGetClient().getDirectoryContents(`${davRootPath}${path}`, { details: true })
+	 */
+	const result: FileStat = {
+		filename: '/files/test/New folder/Neue Textdatei.md',
+		basename: 'Neue Textdatei.md',
+		lastmod: 'Tue, 25 Jul 2023 12:29:34 GMT',
+		size: 123,
+		type: 'file',
+		etag: '7a27142de0a62ed27a7293dbc16e93bc',
+		mime: 'text/markdown',
+		props: {
+			resourcetype: { collection: false },
+			displayname: 'New File',
+			getcontentlength: '123',
+			getcontenttype: 'text/markdown',
+			getetag: '"7a27142de0a62ed27a7293dbc16e93bc"',
+			getlastmodified: 'Tue, 25 Jul 2023 12:29:34 GMT',
+		},
+	}
+
+	test('path does not contain root', () => {
+		const node = davResultToNode(result)
+		expect(node.basename).toBe(result.basename)
+		expect(node.extension).toBe('.md')
+		expect(node.source).toBe('https://localhost/dav/files/test/New folder/Neue Textdatei.md')
+		expect(node.root).toBe(davRootPath)
+		expect(node.path).toBe('/New folder/Neue Textdatei.md')
+		expect(node.dirname).toBe('/New folder')
+		expect(node.size).toBe(123)
+		expect(node.mtime?.getTime()).toBe(Date.parse(result.lastmod))
+		expect(node.mime).toBe(result.mime)
+	})
+
+	test('has correct root set', () => {
+		const remoteResult = { ...result, filename: '/root/New folder/Neue Textdatei.md' }
+		const node = davResultToNode(remoteResult, '/root')
+		expect(node.basename).toBe(remoteResult.basename)
+		expect(node.extension).toBe('.md')
+		expect(node.root).toBe('/root')
+		expect(node.source).toBe('https://localhost/dav/root/New folder/Neue Textdatei.md')
+		expect(node.path).toBe('/New folder/Neue Textdatei.md')
+		expect(node.dirname).toBe('/New folder')
+	})
+
+	test('has correct remote path set', () => {
+		const remoteResult = { ...result, filename: '/root/New folder/Neue Textdatei.md' }
+		const node = davResultToNode(remoteResult, '/root', 'http://example.com/dav')
+		expect(node.basename).toBe(remoteResult.basename)
+		expect(node.extension).toBe('.md')
+		expect(node.source).toBe('http://example.com/dav/root/New folder/Neue Textdatei.md')
+		expect(node.path).toBe('/New folder/Neue Textdatei.md')
+		expect(node.dirname).toBe('/New folder')
 	})
 })
 
