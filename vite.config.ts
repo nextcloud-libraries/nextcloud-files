9a06e5e2481222fa94e84cb4d33a99168841a233
skjnldsv <skjnldsv@protonmail.com>
2024-05-15 12:00:30 +0200

    feat(ci): add codecov bundler
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { codecovVitePlugin } from '@codecov/vite-plugin'
 import { createLibConfig } from '@nextcloud/vite-config'
 
 export default createLibConfig({
@@ -7,5 +8,16 @@ export default createLibConfig({
 	nodeExternalsOptions: {
 		// Force bundle pure ESM module
 		exclude: ['is-svg'],
+	},
+
+	config: {
+		plugins: [
+			// Put the Codecov vite plugin after all other plugins
+			codecovVitePlugin({
+				enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+				bundleName: '@nextcloud/files',
+				uploadToken: process.env.CODECOV_TOKEN,
+			}),
+		],
 	},
 })
