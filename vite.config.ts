4d0763d4e131937d9dd23be6e44930c384de071a
Ferdinand Thiessen <opensource@fthiessen.de>
2026-07-07 17:48:48 +0200

    chore: adjust vite config
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,11 @@
  * SPDX-FileCopyrightText: 2023-2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: CC0-1.0
  */
-import { codecovVitePlugin } from '@codecov/vite-plugin'
+
 import { createLibConfig } from '@nextcloud/vite-config'
 
 export default createLibConfig({
 	index: 'lib/index.ts',
 	dav: 'lib/dav/index.ts',
 	upload: 'lib/upload/index.ts',
-}, {
-	config: {
-		plugins: [
-			// Put the Codecov vite plugin after all other plugins
-			codecovVitePlugin({
-				enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-				bundleName: '@nextcloud/files',
-				uploadToken: process.env.CODECOV_TOKEN,
-			}),
-		],
-	},
 })
