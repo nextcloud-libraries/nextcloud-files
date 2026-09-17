a421a88ef84d937e1cddda9abc6d62fc11c118ad
Ferdinand Thiessen <opensource@fthiessen.de>
2026-09-17 17:41:30 +0200

    test: consolidate testing logic
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- __mocks__/@nextcloud/router.js
+++
@@ -1,9 +0,0 @@
-/*!
- * SPDX-FileCopyrightText: 2022-2024 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-
-/**
- * @param {string} path The path
- */
-export const generateRemoteUrl = (path) => `https://localhost/${path}`
--- __tests__/dav/public-shares.spec.ts
+++ __tests__/dav/public-shares.spec.ts
@@ -4,72 +4,33 @@
  */
 
 import type { FileStat } from 'webdav'
-import type { resultToNode as IResultToNode } from '../../lib/dav/dav.ts'
 
-import { beforeEach, describe, expect, test, vi } from 'vitest'
+import { beforeAll, describe, expect, test } from 'vitest'
+import { getRemoteURL, getRootPath, resultToNode } from '../../lib/dav/dav.ts'
+import { setPublicShare } from '../helpers.ts'
 
-const getCurrentUser = vi.hoisted(() => (vi.fn()))
-const router = vi.hoisted(() => ({ generateRemoteUrl: vi.fn() }))
-const sharing = vi.hoisted(() => ({ isPublicShare: vi.fn(), getSharingToken: vi.fn() }))
+beforeAll(() => {
+	window._oc_webroot = ''
+	setPublicShare('token-1234')
+})
 
-vi.mock('@nextcloud/auth', async (original) => ({
-	...(await original()),
-	getCurrentUser,
-}))
-
-vi.mock('@nextcloud/router', () => router)
-vi.mock('@nextcloud/sharing/public', () => sharing)
-
-function restoreMocks() {
-	vi.resetAllMocks()
-	router.generateRemoteUrl.mockImplementation((service) => `https://example.com/remote.php/${service}`)
-}
-
-function mockPublicShare() {
-	getCurrentUser.mockImplementationOnce(() => null)
-	sharing.isPublicShare.mockImplementation(() => true)
-	sharing.getSharingToken.mockImplementation(() => 'token-1234')
-}
-
-describe('DAV path functions', () => {
-	beforeEach(() => {
-		vi.resetModules()
-		restoreMocks()
-	})
-
-	test('root path is correct on public shares', async () => {
-		mockPublicShare()
-
-		const { getRootPath } = await import('../../lib/dav/dav.ts')
+describe('DAV path functions on public shares', () => {
+	test('root path is correct', () => {
 		expect(getRootPath()).toBe('/files/token-1234')
 	})
 
-	test('remote URL is correct on public shares', async () => {
-		mockPublicShare()
-
-		const { getRemoteURL } = await import('../../lib/dav/dav.ts')
-		expect(getRemoteURL()).toBe('https://example.com/public.php/dav')
+	test('remote URL is correct', () => {
+		expect(getRemoteURL()).toBe(`${window.location.origin}/public.php/dav`)
 	})
 })
 
-describe('on public shares', () => {
-	beforeEach(() => {
-		vi.resetAllMocks()
-		vi.resetModules()
-	})
-
-	// Wrapper function as we can not static import the function to allow mocking the modules
-	const resultToNode = async (...rest: Parameters<typeof IResultToNode>) => {
-		const { resultToNode } = await import('../../lib/dav/dav.ts')
-		return resultToNode(...rest)
-	}
-
+describe('resultToNode on public shares', () => {
 	/*
 	* Result of:
 	* davGetClient().getDirectoryContents(`${davRootPath}${path}`, { details: true })
 	*/
 	const result: FileStat = {
-		filename: '/files/test/New folder/Neue Textdatei.md',
+		filename: '/root/New folder/Neue Textdatei.md',
 		basename: 'Neue Textdatei.md',
 		lastmod: 'Tue, 25 Jul 2023 12:29:34 GMT',
 		size: 123,
@@ -86,20 +47,10 @@ describe('on public shares', () => {
 		},
 	}
 
-	describe('resultToNode', () => {
-		beforeEach(() => {
-			vi.resetModules()
-			restoreMocks()
-		})
+	test('has correct owner set', () => {
+		const node = resultToNode(result, '/root', 'http://example.com/remote.php/dav')
 
-		test('has correct owner set on public shares', async () => {
-			mockPublicShare()
-
-			const remoteResult = { ...result, filename: '/root/New folder/Neue Textdatei.md' }
-			const node = await resultToNode(remoteResult, '/root', 'http://example.com/remote.php/dav')
-
-			expect(node.isDavResource).toBe(true)
-			expect(node.owner).toBe('anonymous')
-		})
+		expect(node.isDavResource).toBe(true)
+		expect(node.owner).toBe('anonymous')
 	})
 })
